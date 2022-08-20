08b5d915b240b4602a88fa34e0f14fa613fb4ece
Bratah123 <brandonnguyen3301@gmail.com>
2022-08-20 16:24:33 -0700

    Init

--- YugimonTheSpire/src/main/java/ModInitializer.java
+++ YugimonTheSpire/src/main/java/ModInitializer.java
@@ -1,20 +1,20 @@
+import basemod.BaseMod;
+import basemod.interfaces.ISubscriber;
+import basemod.interfaces.PostDrawSubscriber;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 
-//When all External Libraries are added from the pom.xml using maven the code can be uncommented.
-
-//import basemod.BaseMod;
-//import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
-
-//@SpireInitializer
-public class ModInitializer {
+@SpireInitializer
+public class ModInitializer implements PostDrawSubscriber {
 
 
-    public ModInitializer(){
-        //Use this for when you subscribe to any hooks offered by BaseMod.
-        //BaseMod.subscribe(this)
+    public ModInitializer() {
+        // Use this for when you subscribe to any hooks offered by BaseMod.
+        BaseMod.subscribe((ISubscriber) this);
     }
 
     //Used by @SpireInitializer
-    public static void initialize(){
+    public static void initialize() {
 
         //This creates an instance of our classes and gets our code going after BaseMod and ModTheSpire initialize.
         ModInitializer modInitializer = new ModInitializer();
@@ -23,4 +23,9 @@ public class ModInitializer {
 
     }
 
+    @Override
+    public void receivePostDraw(AbstractCard abstractCard) {
+        System.out.println("Player just drew card: " + abstractCard.name);
+        abstractCard.damage = 99;
+    }
 }
